221ad6531201385243cb1226162d1305bd44a7d9
Vinicyus Campestrini <54412323+VinicyusCampestrini@users.noreply.github.com>
2019-10-04 11:14:51 -0300

    Revert "Aula 92"
    This reverts commit fad7ca9e

--- lib/pages/login_api.dart
+++ lib/pages/login_api.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:novoprojetoflutter/pages/usuario.dart';
 
 class LoginApi {
-  static Future<Usuario> login(String entrar, String senha) async {
+  static Future<bool> login(String entrar, String senha) async {
     var url = 'https://carros-springboot.herokuapp.com/api/v2/login';
 
     Map<String,String> headers = {
@@ -22,11 +21,6 @@ class LoginApi {
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
 
-    Map mapResponse = json.decode(response.body);
-
-    final user = Usuario.fromJson(mapResponse);
-
-    return user;
-
+    return true;
   }
 }
